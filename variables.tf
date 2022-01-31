a416b866e20297076f9b15f96bb47b0c899238b4
unknown <selbiger10@gmail.com>
2022-01-31 14:09:00 +0200

    Removed type

--- variables.tf
+++ variables.tf
@@ -2,6 +2,7 @@ variable "db_identifier_name" {
   type        = string
   description = "DB Identifier name"
 }
+
 variable "engine_version" {
   type        = string
   description = "DB Engine Version"
@@ -26,7 +27,6 @@ variable "subnets_ids" {
 }
 
 variable "rds_ingress_ports" {
-  type        = list(string)
   description = "Postgres RDS ingress ports"
   default     = [5432]
 }
