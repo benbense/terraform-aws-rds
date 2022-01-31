c94d6831fe43c3029dfe19339fb17c73fb4c388a
unknown <selbiger10@gmail.com>
2022-01-31 15:49:24 +0200

    Added more outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "rds_endpoint" {
   description = "RDS Connection Endpoint"
   value       = aws_db_instance.postgres_db.endpoint
 }
+
+output "rds_address" {
+  description = "RDS Host address"
+  value       = aws_db_instance.postgres_db.address
+}
+
+output "rds_port" {
+  description = "RDS Port"
+  value       = aws_db_instance.postgres_db.port
+}
