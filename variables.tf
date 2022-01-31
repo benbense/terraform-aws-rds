e08a2020f970dba95b0e01388e53196582d00464
unknown <selbiger10@gmail.com>
2022-01-31 13:31:21 +0200

    Removed not used variables

--- variables.tf
+++ variables.tf
@@ -3,21 +3,6 @@ variable "rds_ingress_ports" {
   description = "Postgres RDS ingress ports"
 }
 
-variable "vpc_workspace_name" {
-  type        = string
-  description = "VPC Workspace Name for Backed State"
-}
-
-variable "servers_workspace_name" {
-  type        = string
-  description = "Servers Workspace Name for Backed State"
-}
-
-variable "tfe_organization_name" {
-  type        = string
-  description = "TFE Organization Name"
-}
-
 variable "db_identifier_name" {
   type        = string
   description = "DB Identifier name"
