61a257ba5c5ab64b014c442582348134e24832e7
unknown <selbiger10@gmail.com>
2022-03-15 16:04:59 +0200

    Removed Validation

--- variables.tf
+++ variables.tf
@@ -33,11 +33,8 @@ variable "rds_ingress_ports" {
 
 variable "db_password" {
   description = "DB Password"
-  validation {
-    condition     = length(var.db_password) >= 8
-    error_message = "DB Password must be at least 8 characters"
-  }
 }
+
 variable "route53_zone_id" {
   description = "Hosted Zone ID"
   type        = string
